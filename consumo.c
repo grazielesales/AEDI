18db5366147fb9f0da3650eb06db062adc782d84
Graziele Sales <99915339+grazielesales@users.noreply.github.com>
2024-05-16 17:49:56 -0300

    Update consumo.c

--- consumo.c
+++ consumo.c
@@ -3,15 +3,19 @@
 #include <stdio.h>
  
 int main() {
- 
+
+//Declarar variavéis
    int x;
    float y, ConsumoMedio;
-   
+
+//Pedir valores
    scanf("%i", &x);
    scanf("%f", &y);
    
+//Calculo do gasto médio de combustivel
    ConsumoMedio = (float) x / y;
    
+//Imprimir resultado
    printf("%0.3f km/l\n" , ConsumoMedio);
    
     return 0;
